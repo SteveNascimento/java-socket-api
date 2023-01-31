fabd2db20d9a232d53f0630cab79ef4ddd465723
SteveNascimento <stevecraken@gmail.com>
2023-01-31 10:26:31 -0300

    Web socket stomp complete

--- src/main/java/com/example/messagingstompwebsocket/configuration/CorsConfigurationWebSocket.java
+++ src/main/java/com/example/messagingstompwebsocket/configuration/CorsConfigurationWebSocket.java
@@ -1,23 +1,20 @@
 package com.example.messagingstompwebsocket.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
-public class CorsConfigurationWebSocket {
+@EnableWebMvc
+public class CorsConfigurationWebSocket implements WebMvcConfigurer {
 
-    @Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
-			}
-		};
+    @Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("*")
+            .allowedHeaders("*")
+			.allowedMethods("*");
 	}
 
-    
 }
