3dd7d460272371b83579cb3b02b6a8f963351d39
SteveNascimento <steve.melo.nascimento@gmail.com>
2023-07-16 10:23:16 -0300

    Alterando versão Java e corrigindo CORS

--- src/main/java/com/example/messagingstompwebsocket/configuration/WebSocketConfig.java
+++ src/main/java/com/example/messagingstompwebsocket/configuration/WebSocketConfig.java
@@ -2,7 +2,6 @@ package com.example.messagingstompwebsocket.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -19,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/ws-socket-register").setAllowedOriginPatterns("*").withSockJS();
+    registry.addEndpoint("/ws-socket-register").setAllowedOrigins("http://localhost:4200").withSockJS();
   }
 
 }
